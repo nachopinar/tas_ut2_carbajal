dad138814c7dde716e4615c6717872e60896f867
nachopinar <136358447+nachopinar@users.noreply.github.com>
2024-08-22 08:20:32 -0300

    .

--- ta1_3.js
+++ ta1_3.js
@@ -1,17 +1,33 @@
 let arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+function mostrarArreglo() {
+    const arregloDiv = document.getElementById("arregloNumeros");
+    arregloDiv.innerHTML = ""; // Limpiar el contenido previo
+
+    arreglo.forEach(elemento => {
+        const p = document.createElement("p");
+        p.textContent = elemento;
+        arregloDiv.appendChild(p);
+    });
+}
+
+// Añadir el event listener al botón
+document.getElementById("botonMostrar").addEventListener("click", mostrarArreglo);
+
 function getOdds() {
     const impares = arreglo.filter(numero => numero % 2 !== 0);
-    const resultado = document.getElementById("impares");
+    const resultado = document.getElementById("arregloImpares");
 
     resultado.innerHTML = " ";
 
     impares.forEach(numero => {
-        const h1 = document.createElement("h1");
-        h1.textContent = numero;
-        resultado.appendChild(h1);
+        const numaux = document.createElement("numaux");
+        numaux.textContent = numero + " ";
+        resultado.appendChild(numaux);
     })
 }
+
+document.getElementById("botonImpares").addEventListener("click", getOdds);
 
 
 
