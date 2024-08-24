1f821f277386b12cb99fb5dce29b53fc95b4d66c
nachopinar <136358447+nachopinar@users.noreply.github.com>
2024-08-23 23:18:55 -0300

    buscarViejo

--- ta3/ta3_1.js
+++ ta3/ta3_1.js
@@ -32,3 +32,55 @@ function generatePassword() {
 }
 
 document.getElementById("botonContraseña").addEventListener("click", generatePassword);
+
+
+function displayPeople(people) {
+    if (!Array.isArray(people) || people.length === 0) {
+        console.log("El arreglo está vacío o no es un arreglo válido.");
+        return;
+    }
+
+    const peopleContainer = document.getElementById("peopleContainer");
+    peopleContainer.innerHTML = ""; // Limpiar contenido previo
+
+    people.forEach(person => {
+        const personInfo = document.createElement("p");
+        personInfo.textContent = `${person.name}: ${person.age} años`;
+        peopleContainer.appendChild(personInfo);
+    });
+}
+
+
+const people = [
+    { name: "Juan", age: 45 },
+    { name: "Ana", age: 67 },
+    { name: "Luis", age: 34 },
+    { name: "María", age: 78 }
+];
+
+document.getElementById("BotonMostrarPersonas").addEventListener("click", () => displayPeople(people));
+
+
+function findTheOldest(people) {
+    if (!Array.isArray(people) || people.length === 0) {
+        console.log("El arreglo está vacío o no es un arreglo válido.");
+        return null;
+    }
+
+    // saco edades
+    const ages = people.map(person => person.age);
+
+    // Encontrar la mayor edad
+    const maxAge = Math.max(...ages);
+
+    // Filtrar para encontrar la persona con la mayor edad
+    const oldestPeople = people.filter(person => person.age === maxAge);
+
+    const oldestPerson = oldestPeople[0];
+
+
+    document.getElementById("personaMasVieja").innerHTML = `${oldestPerson.name}: ${oldestPerson.age} años`;
+}
+
+
+document.getElementById("BotonMostrarViejo").addEventListener("click", () => findTheOldest(people));
